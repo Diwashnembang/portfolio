ab362769abd2a744d3cffa286fd7c2a4e6319f18
Diwash Nembang <diwashnembang2@gmail.com>
2025-04-27 20:17:43 +1000

    added a animaitn to home text

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,14 +1,16 @@
 import { Github ,Linkedin, Mail, CloudDownload} from "lucide-react"
 import home from "../assets/Home.svg"
+import { Typewriter } from "react-simple-typewriter"
 export function Main(){
     return (
         <>
-        <div className="py-5 md:grid grid-cols-8 items-center [height:100%] pt-20">
+        <div className="pt-10 md:grid grid-cols-8 items-center [height:100%] md:pt-20">
 
         <div className="w-full grid grid-cols-8 gap-1.25 content-center justify-items-center [height:186px] md:col-span-4">
             <div className="[font-size:20px] col-span-8 md:[font-size:40px]">
                 <h1>Hi! I am Diwash Nembang</h1>
-                <h1>i'm a software developer</h1>
+                <Typewriter words={["I'm a student.","I'm a software developer."]} loop={false} cursor={true} cursorBlinking={true}>
+                </Typewriter>
             </div>
             <ul className="[font-size:20px] flex col-span-8  gap-5 justify-items-center items-center h-full my-5">
                 <li ><a className="hover:text-gray-400"><Mail className="[width:20px] [height:20px] md:[width:40px] [height:40px]"></Mail></a></li>
