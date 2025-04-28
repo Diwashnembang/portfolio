e874085f325c11dfe0ec5448098d39b1b600a982
Diwash Nembang <diwashnembang2@gmail.com>
2025-04-28 17:17:07 +1000

    Refactor code structure and remove redundant functions for improved readability and maintainability. added analystic

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {  createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {   createHashRouter,  RouterProvider, Outlet } from 'react-router-dom'
 import { Projects } from './components/Projects.tsx'
 import { Contact } from './components/Contact.tsx'
+import { Analytics } from "@vercel/analytics/react"
 
 
-let router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  {path:"/projects",element:<Projects></Projects>},
-  {path:"/contact", element: <Contact></Contact>}])
+let router = createHashRouter([
+  {
+    element: <RootLayout/>,
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/projects", element: <Projects></Projects> },
+      { path: "/contact", element: <Contact></Contact> }
+    ]
+  }
+])
 
-
+function RootLayout() {
+  return (
+    <>
+      <Analytics />
+      <Outlet />
+    </>
+  )
+}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <RouterProvider router={router}></RouterProvider>
